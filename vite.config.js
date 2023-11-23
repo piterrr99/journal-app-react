b99afc728c132019cf9a59e74a2a65a8a1106669
Pedro Hourrutiner <pedrodavidhourrutinerdiaz@gmail.com>
2023-11-23 09:09:08 -0500

    dinamic deployment configured

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/journal-app-react",
-  plugins: [react()],
-  server: {
-    host: true,
-    cors: true
-  }
+export default defineConfig(({command})=>{
+  const config = {
+    base: "/",
+    plugins: [react()],
+  };
+
+  if (command !== 'serve') {
+    config.base = '/journal-app-react/'
+  };
+  
+  return config;
 });
