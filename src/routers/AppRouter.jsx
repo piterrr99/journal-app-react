367df4e20f43d423773a081f9b8e0fdaa79935f6
Pedro Hourrutiner <pedrodavidhourrutinerdiaz@gmail.com>
2023-11-24 11:39:46 -0500

    AppRouter: auth status kept after reloading page

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,10 +1,30 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import React from 'react'
+import { useDispatch } from 'react-redux';
 
-import { AuthRouter } from "./AuthRouter";
+import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from '../components/journal/JournalScreen';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { FirebaseAuth } from '../firebase/config';
+import { login } from '../store/auth/authSlice';
 
 export const AppRouter = () => {
+
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		
+		onAuthStateChanged(FirebaseAuth, (user)=>{
+			if (user?.uid){
+				dispatch(
+					login( {displayName: user.displayName, email: user.email, uid: user.uid} )
+				)
+			}
+		})
+	}, [])
+  
+
   return (
     
     <BrowserRouter basename= {import.meta.env.DEV ? '/' : '/journal-app-react'}>
